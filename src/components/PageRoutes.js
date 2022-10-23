26b602a39b8b6547bada91cda2fb6ba59dc51968
mwai <mwaijohng@gmail.com>
2022-10-23 10:24:11 +0300

    d

--- src/components/PageRoutes.js
+++ src/components/PageRoutes.js
@@ -7,9 +7,9 @@ import Login from './Login'
 function PageRoutes() {
     return (
         <Routes>
-            <Route path='/funsoft/login' element={<Login/>} />
+            <Route path='funsoft/login' element={<Login/>} />
             <Route path='funsoft/' element={<Login/>} />
-            <Route path='/funsoft/cash-collection' element={<ComponentsWithSidebar>
+            <Route path='funsoft/cash-collection' element={<ComponentsWithSidebar>
                 <CashCollection />
             </ComponentsWithSidebar>} />
         </Routes>
